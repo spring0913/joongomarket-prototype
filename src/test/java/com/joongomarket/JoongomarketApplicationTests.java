cac4efe7ba346708997f16c4206a58184f1699e1
spring0913 <bbomi0913@gmail.com>
2021-09-02 01:00:19 +0900

    환경 세팅

---
+++ src/test/java/com/joongomarket/JoongomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoongomarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
