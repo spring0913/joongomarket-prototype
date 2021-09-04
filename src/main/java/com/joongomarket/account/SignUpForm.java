50c60bdfb898d3a96230f931a1ab0ac2ae13b372
spring0913 <bbomi0913@gmail.com>
2021-09-04 23:32:23 +0900

    회원 가입 view 추가

---
+++ src/main/java/com/joongomarket/account/SignUpForm.java
@@ -0,0 +1,13 @@
+package com.joongomarket.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
