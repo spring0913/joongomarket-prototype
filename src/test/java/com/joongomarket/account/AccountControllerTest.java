5750411ba871147dea280e80001956128568b947
spring0913 <bbomi0913@gmail.com>
2021-09-15 14:13:56 +0900

    회원 가입 인증 메일 확인

--- src/test/java/com/joongomarket/account/AccountControllerTest.java
+++ src/test/java/com/joongomarket/account/AccountControllerTest.java
@@ -66,6 +66,7 @@ class AccountControllerTest {
         Account account = accountRepository.findByEmail("bmsung@naver.com");
         assertNotNull(account);
         assertNotEquals(account.getPassword(), "12345678");
+        assertNotNull(account.getEmailCheckToken());
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
