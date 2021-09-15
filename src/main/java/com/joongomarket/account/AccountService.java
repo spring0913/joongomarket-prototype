837b4aa53c7d6baa16dc20848237121cb6a6ef33
spring0913 <bbomi0913@gmail.com>
2021-09-15 10:58:53 +0900

    회원 가입 패스워드 encoding

--- src/main/java/com/joongomarket/account/AccountService.java
+++ src/main/java/com/joongomarket/account/AccountService.java
@@ -4,6 +4,7 @@ import com.joongomarket.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
@@ -23,7 +25,7 @@ public class AccountService {
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
                 .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword()) // TODO password encoding
+                .password(passwordEncoder.encode(signUpForm.getPassword()))
                 .auctionCreatedByWeb(true)
                 .auctionUpdatedByWeb(true)
                 .build();
