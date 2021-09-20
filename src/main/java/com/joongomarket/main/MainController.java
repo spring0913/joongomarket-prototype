01ae96ae0ef47930db3adc1a90434e9f6226b72d
spring0913 <bbomi0913@gmail.com>
2021-09-20 21:02:52 +0900

    인증된 사용자 정보 참조

---
+++ src/main/java/com/joongomarket/main/MainController.java
@@ -0,0 +1,20 @@
+package com.joongomarket.main;
+
+import com.joongomarket.account.CurrentUser;
+import com.joongomarket.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model){
+        if (account != null){
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
